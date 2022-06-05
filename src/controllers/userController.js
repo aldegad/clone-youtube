6be384f206b2db963f403cce3cec2a311e7cc017
aldegad <aldegad@naver.com>
2022-06-06 02:08:53 +0900

    login end

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model";
+import bcrypt from 'bcrypt';
 
 export const getJoin = (req, res, next) => res.render("Join", { pageTitle: "Join" });
 export const postJoin = async(req, res, next) => {
@@ -27,11 +28,31 @@ export const postJoin = async(req, res, next) => {
     }
     catch(e) {
         console.log("❌ User Join Error", e);
-        return res.render("Join", { pageTitle: "Join", errorMessage: e });
+        return res.status(400).render("Join", { pageTitle: "Join", errorMessage: e });
     }
 }
-export const login = (req, res, next) => res.render("login", { pageTitle: "Login" });
+
+
+export const getLogin = (req, res, next) => res.render("login", { pageTitle: "Login" });
+export const postLogin = async(req, res, next) => {
+    const pageTitle = "Login";
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if(!user) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "An account with this username does not exist." });
+    }
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "Wrong password." });
+    }
+    
+    return res.redirect("/");
+}
+
+
 export const logout = (req, res, next) => res.send("Log Out");
+
+
 export const see = (req, res, next) => res.send("See user");
 export const edit = (req, res, next) => res.send("Edit user");
 export const deleteUser = (req, res, next) => res.send("Remove user");
